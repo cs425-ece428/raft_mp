218da0fce011ab244f5237485771ff7338b9e297
Raj Kishore <rajkish321@example.com>
2021-11-08 17:02:40 -0600

    finished reader without dealing with partitions

--- raft.py
+++ raft.py
@@ -1,6 +1,18 @@
 import time
 import sys
 
+#constants
+TERM = 1
+STATE = 2
+LEADER = 3
+LOG = 4
+COMMIT_INDEX = 5
+
+REQUEST_VOTE = 1
+VOTE = 2
+HEARTBEAT = 3
+
+
 pid = int(sys.argv[1])
 n = int(sys.argv[2])
 last = None
@@ -16,14 +28,82 @@ def parse_message(message: str):
     , sender_id, action, args = message.split(" ", 3)
     return sender_id, message, args
 
+current_votes = 0
+last_term_voted = -1
+
 def reader(message: str):
     sender_id, message, args = parse_message(message)
+    
+    message_term = args.split(" ")[-1]
+
+
     if message == "RequestVote":
         print("received request vote")
+
+        if state["state"] == "FOLLOWER":
+            if message_term > last_term_voted:
+                #only vote if have not voted for this term (only higher terms, no need for lower terms b/c irrelevant)
+                write(VOTE, sender_id, "True") #send a True vote message to sender
+                last_term_voted = message_term
+            else:
+                write(VOTE, sender_id, "False") #send a False vote message to sender
+
+
+        else: 
+            #if candidate or leader
+            if message_term > state["term"]:
+                #only vote if term higher than current state term
+                write(VOTE, sender_id, "True") #send a True vote message to sender
+                #i don't think we update state term yet
+            else:
+                write(VOTE, sender_id, "False") #send a False vote message to sender
+
+
+
     if message == "Vote":
         print("received a vote")
+        decision = args.split(" ")[-2] 
+
+        if state["state"] != "CANDIDATE":
+            #if somehow state changed from candidate to follower/leader in between sending/receiving, ignore
+            #Vote is only used by processes in candidate state
+            return
+
+        elif decision == "True":
+            
+            current_votes += 1
+            if current_votes > n/2:
+                #become leader if majority of votes
+                
+                # or just use the heartbeat thread 
+                for i in range(n):
+                    if i != pid: # no need to send heartbeat to self
+                        write(HEARTBEAT, int(i))
+
+
+
+                update_state(STATE, "LEADER") #change state to leader
+                update_state(LEADER, pid) #set leader to itself
+        
+
+
     if message == "Heartbeat":
         print ("received a heartbeat")
+        if state["state"] == "LEADER":
+            # only respond if other leader has higher term, this would be caused by partitioning (unsure of specifics)
+            return
+        else:
+            #we should only update this stuff if message_term > state["term"] i think, but this is partitioning again
+            
+            update_state(STATE, "FOLLOWER") #make itself follower if not already set
+            update_state(LEADER, sender_id) #make sender the leader if not already set
+            update_state(TERM, message_term) #update term to whatever was sent in message 
+            write(HEARTBEAT, sender_id)
+
+            #will need to add updates for log messages in CP2
+        
+
+
 
 
 while True:
@@ -44,7 +124,7 @@ state = {
     "commit_index" = 0
 }
 logs = []
-def write(request_type, receiver_id, msg):
+def write(request_type, receiver_id, msg = ""):
 
     if request_type == 1:
         print("SEND " + str(receiver_id) + " RequestVote " + str(msg) + " " + state[term])
@@ -66,6 +146,9 @@ def update_state(state_var, new_value):
         if state["state"] != new_value:
             print("STATE state=" + new_value)
             state["state"] = new_value
+
+        if new_value == "FOLLOWER":
+            current_votes = 0
         
     if state_var == 3:
         #leader  
